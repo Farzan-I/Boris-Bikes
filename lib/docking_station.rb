adc252d9743f792227e3c5079cd56b6ef6c8c5b6
Farzan-I <farzan_15@hotmail.co.uk>
2022-04-21 14:55:06 +0100

    Bike spec and Bike file

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,12 +1,16 @@
 class DockingStation
-  def initialize(bike)
-    @bike = bike
-  end
+  # def initialize(bike)
+  #   @bike = bike
+  # end
 
   def release_bike()
 
   end
+
+  # def 
+  #   # working
+  # end
   
 end
 
-docking_station = DockingStation.new
+# docking_station = DockingStation.new
